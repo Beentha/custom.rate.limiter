e6d4de75215ce16eb87c38db6164fd2f0c0c65ee
binta.umar <binta.umar@teamapt.com>
2024-06-04 17:55:58 +0100

    fix exception error

--- src/main/java/com/rate/limit/exceptions/RateLimitException.java
+++ src/main/java/com/rate/limit/exceptions/RateLimitException.java
@@ -1,12 +1,10 @@
 package com.rate.limit.exceptions;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class RateLimitException extends RuntimeException {
 
     private final long retryAfterSeconds;
