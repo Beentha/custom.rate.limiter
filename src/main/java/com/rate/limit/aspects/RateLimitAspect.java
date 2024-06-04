16e1aea0d13f1c9ac31461f230258430b1a303ed
binta.umar <binta.umar@teamapt.com>
2024-06-04 18:24:09 +0100

    added tests

--- src/main/java/com/rate/limit/aspects/RateLimitAspect.java
+++ src/main/java/com/rate/limit/aspects/RateLimitAspect.java
@@ -6,14 +6,19 @@ import com.rate.limit.rate_limit.model.request.RateLimitRequest;
 import com.rate.limit.rate_limit.service.RateLimitService;
 import com.rate.limit.user_token.service.UserTokenExecutor;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
+@Aspect
 @RequiredArgsConstructor
 public class RateLimitAspect {
 
     private final UserTokenExecutor tokenExecutor;
     private final RateLimitService rateLimitService;
 
-    public void isValid(RateLimiter rateLimiter) {
+    @Before(value = "@annotation(rateLimiter)")
+    public void isValid(JoinPoint joinPoint, RateLimiter rateLimiter) {
         String token = tokenExecutor.getToken(rateLimiter.tokenType());
         RateLimitRequest request = new RateLimitRequest(
                 rateLimiter.resource(),
